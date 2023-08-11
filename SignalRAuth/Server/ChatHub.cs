181a89ac9aa475eccb0b88497e583108c92f270c
Jack Reimers <jack.reimers.00@gmail.com>
2023-08-11 15:43:00 +1000

    Add streaming authorisation

--- SignalRAuth/Server/ChatHub.cs
+++ SignalRAuth/Server/ChatHub.cs
@@ -1,14 +1,35 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using SignalRAuth.Shared;
 
 namespace SignalRAuth.Server;
 
-[Authorize]
 public class ChatHub : Hub
 {
+    [Authorize]
     public async Task SendMessage(ChatMessage message)
     {
         await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+    
+    public async Task SendMessageNoAuth(ChatMessage message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+
+    [Authorize]
+    public async IAsyncEnumerable<int> StreamCount([EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        var count = 10;
+        var delay = 1000;
+        
+        for (var i = 0; i < count; i++)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            yield return i;
+
+            await Task.Delay(delay, cancellationToken);
+        }
     }
 }
